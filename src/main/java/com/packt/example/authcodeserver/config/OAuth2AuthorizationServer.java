3463ab306f24e86c001d3de62333a91b8fa971f3
peterxiao1234 <peter_xiao@hotmail.com>
2018-09-20 12:58:03 -0500

    use password and no url redirect

--- src/main/java/com/packt/example/authcodeserver/config/OAuth2AuthorizationServer.java
+++ src/main/java/com/packt/example/authcodeserver/config/OAuth2AuthorizationServer.java
@@ -1,17 +1,34 @@
 package com.packt.example.authcodeserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 /**
  * Created by peter.xiao on 9/19/2018.
  */
+
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2AuthorizationServer extends
         AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+			 throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients)
@@ -20,11 +37,10 @@ public class OAuth2AuthorizationServer extends
         clients.inMemory()
             .withClient("clientapp")
             .secret("123456")
-                .redirectUris("http://localhost:9000/callback")
-                .authorizedGrantTypes("implicit")
-                .accessTokenValiditySeconds(120)
+                .authorizedGrantTypes("password")
                 .scopes("read_profile", "read_contacts");
         //@formatter:on
     }
+
 
 }
