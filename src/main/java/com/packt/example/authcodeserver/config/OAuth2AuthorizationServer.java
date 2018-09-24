826bdd9ebec98c7777a0deb70426462ff3358327
peterxiao1234 <peter_xiao@hotmail.com>
2018-09-24 10:34:25 -0500

    save for oauth2server

--- src/main/java/com/packt/example/authcodeserver/config/OAuth2AuthorizationServer.java
+++ src/main/java/com/packt/example/authcodeserver/config/OAuth2AuthorizationServer.java
@@ -21,7 +21,6 @@ import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import javax.sql.DataSource;
 
@@ -34,32 +33,36 @@ import javax.sql.DataSource;
 public class OAuth2AuthorizationServer extends
         AuthorizationServerConfigurerAdapter {
     @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private RedisConnectionFactory connectionFactory;
-    @Override
-    public void configure(
-		AuthorizationServerEndpointsConfigurer endpoints)
-            throws Exception {
-        endpoints
-                .authenticationManager(authenticationManager)
-                .tokenStore(tokenStore());
-    }
-    @Bean
-    public TokenStore tokenStore() {
-        RedisTokenStore redis = new RedisTokenStore(connectionFactory);
-        return redis;
-    }
+    private DataSource dataSource;
     @Override
     public void configure(ClientDetailsServiceConfigurer clients)
             throws Exception {
-        clients.inMemory()
-                .withClient("clientapp").secret("123456")
-                .authorizedGrantTypes("password", "authorization_code")
-                .scopes("read_profile", "read_contacts");
+        clients.jdbc(dataSource);
     }
-
-
-
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+    @Bean
+    public ApprovalStore approvalStore() {
+        return new JdbcApprovalStore(dataSource);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(4);
+    }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer
+                                  endpoints)
+            throws Exception {
+        endpoints
+                .approvalStore(approvalStore())
+                .tokenStore(tokenStore());
+    }
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer
+                                  security)
+            throws Exception {
+        security.passwordEncoder(passwordEncoder());
+    }
 }
